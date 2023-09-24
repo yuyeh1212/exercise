ea8baaa91b7b65c09e74f98b3eb6a455c5256e8a
yuyeh <herry861212@gmail.com>
2023-09-24 11:19:06 +0800

    修正

--- additional/app_01.js
+++
@@ -1,9 +0,0 @@
-const friends = ["Harry", "Ron", "Shap", "Mike", "Grace"];
-
-for (let i = 0; i < friends.length / 2; i++) {
-  let temp = friends[i];
-  friends[i] = friends[friends.length - 1 - i];
-  friends[friends.length - 1 - i] = temp;
-}
-
-console.log(friends);
